d7442f8d9183c519de578e96724be1c96e807679
abinetermias <abinet.gobena2022@gmail.com>
2022-06-09 01:11:49 -0700

    Task-5 update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
- * main - print all single digit number of basr 10 starting from 0,
- * followed by a new line, using putchar
- * Return: Always 0 (success)
+ * main-prints all single digit number of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Sucess)
  */
-in main (void)
+int main(void)
 {
 	int n;
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
+	for (n = 0; n < 10; n++)
+		printf("%d" , n);
+	printf("\n");
 	return (0);
 }
