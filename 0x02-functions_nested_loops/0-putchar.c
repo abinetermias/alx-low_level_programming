550af3e0d81e969ffc4b33044f87b7d93563fc42
abinetermias <abinet.gobena2022@gmail.com>
2022-09-14 17:48:31 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,15 @@
+#include <unistd.h>
 #include "main.h"
-
 /**
  * main - print putchar
  * Return: Always 0 (Success)
  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 
 int main(void)
 {
