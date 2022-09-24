0f50af7f75e0043222f25622fcb603eaeace1331
abinetermias <abinet.gobena2022@gmail.com>
2022-09-23 17:03:38 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -> multiplying 2 numbers
+ * @a: intiger param1
+ * @b: intiger param 2
+ * Return: Success and intiger
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
