7e883ea4384ada37bc5fede7bdb7f7e385689255
abinetermias <abinet.gobena2022@gmail.com>
2022-10-05 06:46:48 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,7 +14,7 @@ void *_memset(void *s, int c, size_t n)
 	unsigned int index;
 	unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)							memory[index] = value;
-
+	for (index = 0; index < n; index++)			
+		memory[index] = value;
 	return (memory);
 }
