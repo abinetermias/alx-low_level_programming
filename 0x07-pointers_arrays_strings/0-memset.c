eec9010f2ecb54302df1170a3d4d3b48ffbc0671
abinetermias <abinet.gobena2022@gmail.com>
2022-10-07 01:01:55 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,21 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * _memset -> this memory set function
  * @s: string
  * @b: a charcter
  * @n: an integer
- * Return: a string
+ * Return: Always 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (memset(s, b, n));
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
 
