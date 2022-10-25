88860474be9756b5c4767f745c2de4835097677b
abinetermias <abinet.gobena2022@gmail.com>
2022-10-25 02:21:17 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 
@@ -33,4 +33,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif 
+#endif
