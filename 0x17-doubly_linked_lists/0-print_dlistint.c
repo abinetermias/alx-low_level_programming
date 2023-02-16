7f7e4aa3e01e655f77c887408470ba72564f0240
abinetermias <abinet.gobena2022@gmail.com>
2023-02-16 03:31:15 -0800

    Task -1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,20 +10,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 
-	int counter = 0;
+size_t nodes = 0;
 
-	if (h == NULL)
-		return (counter);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		counter++;
 	}
 
-	return (counter);
+	return (nodes);
 }
